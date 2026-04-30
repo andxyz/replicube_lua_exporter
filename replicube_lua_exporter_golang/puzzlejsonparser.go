5a76babc885f8908688f513f341c683956ef5627
Andrew Stevens <andxyz.stevens@gmail.com>
2026-04-30 15:22:41 -0400

    Adds exporting all weekly puzzles

--- replicube_lua_exporter_golang/puzzlejsonparser.go
+++ replicube_lua_exporter_golang/puzzlejsonparser.go
@@ -11,6 +11,7 @@ import (
 
 func sanitizeDirString(dirName string) string {
 	dirName = strings.ReplaceAll(dirName, " ", "_")
+	dirName = strings.ReplaceAll(dirName, ".", "_")
 	reg := regexp.MustCompile(`[^0-9A-Za-z_]`)
 	dirName = reg.ReplaceAllString(dirName, "")
 	reg2 := regexp.MustCompile(`_+$`)
@@ -21,12 +22,18 @@ func sanitizeDirString(dirName string) string {
 func ParsePuzzleJSONCreateDirsAndLuaFiles(jsonData []byte, outputDir string) error {
 	var puzzlesData PuzzlesData
 	err := json.Unmarshal(jsonData, &puzzlesData)
+
+	if outputDir == "" {
+		return fmt.Errorf("output directory must be specified")
+	}
+
 	if err != nil {
 		return fmt.Errorf("error unmarshalling JSON: %w", err)
 	}
 
 	for _, puzzle := range puzzlesData.Puzzles {
-		if puzzle.Source != 100 {
+		if !isMainStoryPuzzle(puzzle) && !isWeeklyPuzzle(puzzle) {
+			fmt.Printf("Warning: skipping puzzle ID: '%s', puzzle source: '%d'\n", puzzle.ID, puzzle.Source)
 			continue
 		}
 
@@ -34,21 +41,24 @@ func ParsePuzzleJSONCreateDirsAndLuaFiles(jsonData []byte, outputDir string) err
 		fmt.Printf("%s:\n", puzzle.ID)
 		fmt.Println("#############")
 
-		levelLookup, ok := properLevelNameLookup[puzzle.ID]
-		if !ok {
-			fmt.Printf("Warning: puzzle ID %s not found in lookup table, skipping\n", puzzle.ID)
-			continue
-		}
-		cleanLevelDirname := sanitizeDirString(levelLookup.Dirname)
-		cleanLevelName := sanitizeDirString(levelLookup.LevelName)
-		targetDir := cleanLevelName
-		if outputDir != "" {
-			targetDir = filepath.Join(outputDir, cleanLevelDirname, cleanLevelName)
+		ok, targetDir := false, ""
+		if isMainStoryPuzzle(puzzle) {
+			ok, targetDir = lookupMainStoryDirname(puzzle, outputDir)
+			if !ok {
+				fmt.Printf("Warning: puzzle ID %s not found in mainstory lookup table, skipping\n", puzzle.ID)
+				continue
+			}
+		} else if isWeeklyPuzzle(puzzle) {
+			ok, targetDir = lookupWeeklyDirname(puzzle, outputDir)
+			if !ok {
+				fmt.Printf("Warning: skipping puzzle ID %s\n", puzzle.ID)
+				continue
+			}
 		}
 
 		err := os.MkdirAll(targetDir, 0755)
 		if err != nil {
-			return fmt.Errorf("error creating directory %s: %w", targetDir, err)
+			return fmt.Errorf("error creating directory '%s': %w", targetDir, err)
 		}
 
 		for i, codeTabName := range puzzle.VariantOrder {
@@ -67,4 +77,31 @@ func ParsePuzzleJSONCreateDirsAndLuaFiles(jsonData []byte, outputDir string) err
 	}
 
 	return nil
+}
+
+func isMainStoryPuzzle(p Puzzle) bool {
+	return p.Source == 100
+}
+
+func lookupMainStoryDirname(puzzle Puzzle, outputDir string) (bool, string) {
+	levelLookup, ok := properLevelNameLookup[puzzle.ID]
+	if !ok {
+		return false, ""
+	}
+
+	cleanLevelDirname := sanitizeDirString(levelLookup.Dirname)
+	cleanLevelName := sanitizeDirString(levelLookup.LevelName)
+	targetDir := filepath.Join(outputDir, cleanLevelDirname, cleanLevelName)
+	return ok, targetDir
+}
+
+func isWeeklyPuzzle(p Puzzle) bool {
+	return p.Source == 400
+}
+
+func lookupWeeklyDirname(puzzle Puzzle, outputDir string) (bool, string) {
+	cleanWeeklyDirname := sanitizeDirString("__Weekly_Puzzles")
+	cleanPuzzleId := sanitizeDirString(puzzle.ID)
+	targetDir := filepath.Join(outputDir, cleanWeeklyDirname, cleanPuzzleId)
+	return true, targetDir
 }
